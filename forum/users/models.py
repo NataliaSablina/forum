2db5f8ae8bf36c3fdb4156b7af280d0bddab4f5d
OlegKulichik <aleh.kulichyk@innowise-group.com>
2023-02-07 16:22:01 +0100

    rename users table

--- forum/users/models.py
+++ forum/users/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, Text, DateTime
 
 
 class User(Base):
-    __tablename__ = "forum_users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, unique=True)
     email = Column(String)
---
+++ migrations/versions/59d20e5668cf_rename_users_table.py
@@ -0,0 +1,24 @@
+"""rename users table
+
+Revision ID: 59d20e5668cf
+Revises: 9461f68da2b8
+Create Date: 2023-02-07 14:27:28.334421
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '59d20e5668cf'
+down_revision = '9461f68da2b8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
